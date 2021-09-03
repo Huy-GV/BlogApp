2004441582af46875b18dc7e87287d459e8ca9e9
basbaybee <emperorhuyv@gmail.com>
2021-09-03 16:32:31 +1000

    shortened css class logic in razor files

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -3,5 +3,7 @@ const addForm = document.querySelector(".add-form");
 
 addBtn.addEventListener("click", () => {
     addBtn.innerHTML = addBtn.innerHTML == "Make a post !" ? "Cancel" : "Make a post !";
+    addBtn.classList.toggle("add-button");
+    addBtn.classList.toggle("cancel-post");
     addForm.classList.toggle("hidden-element");
 })
