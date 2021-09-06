0dbf910f2877457bd5440f1e4d8f2d5bd48e00cb
basbaybee <emperorhuyv@gmail.com>
2021-09-06 16:55:07 +1000

    added new keyword to override content property, migration not needed

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -10,7 +10,7 @@ namespace BlogApp.Models
     {
         public int ID { get; set; }
         [Required, MaxLength(250)]
-        public string Content { get; set; }
+        public new string Content { get; set; }
         [Required]
         public int BlogID { get; set; }
     }
