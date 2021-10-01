da074d5fa84ce4e5a029f28fd6ff214a0ee5759a
basbaybee <emperorhuyv@gmail.com>
2021-10-01 19:34:44 +1000

    turned logo to home link, restyled admin page, made blog content display line breaks

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,8 @@ namespace BlogApp
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(
+                options => options.SignIn.RequireConfirmedAccount = true)
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
